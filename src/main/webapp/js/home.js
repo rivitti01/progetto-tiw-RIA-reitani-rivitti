81a7ac19ed921de0353ef38079b25c7f0bba7bff
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-07-12 14:15:33 +0200

    attivato il logout

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -1,1 +1,23 @@
 sessionStorage.clear();
+
+{
+    //#####MAPPIAMO LE FUNZIONI AI BOTTONI#####
+
+    // aggiungo la funzione al click del bottone di logout
+    document.getElementById("logout").addEventListener("click", function(){
+        logout();
+    });
+
+    //#####DICHIARIAMO LE FUNZIONI#####
+
+    function logout() {
+        makeCall("GET", 'Logout', null, function (risposta) {
+            if (risposta.readyState === XMLHttpRequest.DONE) {
+                sessionStorage.clear();
+                window.location.href = "index.html";
+            }
+        });
+    }
+
+
+}
